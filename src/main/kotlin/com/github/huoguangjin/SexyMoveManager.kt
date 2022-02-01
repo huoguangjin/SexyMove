2da2f704de23cb8372ab37e67ac09865897e7aa9
hgj <352886006@qq.com>
2022-02-01 18:16:27 +0800

    feat: apply settings to SexyMoveManager

--- src/main/kotlin/com/github/huoguangjin/SexyMoveManager.kt
+++ src/main/kotlin/com/github/huoguangjin/SexyMoveManager.kt
@@ -1,11 +1,14 @@
 package com.github.huoguangjin
 
 import com.intellij.openapi.Disposable
+import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.EditorFactory
 import com.intellij.openapi.editor.event.EditorFactoryEvent
 import com.intellij.openapi.editor.event.EditorFactoryListener
 
 class SexyMoveManager : Disposable, EditorFactoryListener {
+
+  private val settings = SexyMoveSettings.getInstance()
 
   init {
     EditorFactory.getInstance().addEditorFactoryListener(this, this)
@@ -17,10 +20,22 @@ class SexyMoveManager : Disposable, EditorFactoryListener {
       return
     }
 
-    val document = editor.document
-    if (!document.isWritable) {
+    if (shouldActivate(editor)) {
+      // it must be disabled
       SexyMoveAction.toggle(editor)
     }
+  }
+
+  private fun shouldActivate(editor: Editor): Boolean {
+    if (settings.activeAlways) {
+      return true
+    }
+
+    if (settings.activeInReadonlyFiles) {
+      return !editor.document.isWritable
+    }
+
+    return false
   }
 
   override fun dispose() {
