a36abd9c3f2f02aa20ba4dfe29227b792f5708da
hgj <352886006@qq.com>
2022-02-08 14:21:52 +0800

    feat: migrate from component to extension point to support dynamic reloading

--- src/main/kotlin/com/github/huoguangjin/SexyMoveManager.kt
+++ src/main/kotlin/com/github/huoguangjin/SexyMoveManager.kt
@@ -1,18 +1,12 @@
 package com.github.huoguangjin
 
-import com.intellij.openapi.Disposable
 import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.editor.EditorFactory
 import com.intellij.openapi.editor.event.EditorFactoryEvent
 import com.intellij.openapi.editor.event.EditorFactoryListener
 
-class SexyMoveManager : Disposable, EditorFactoryListener {
+class SexyMoveManager : EditorFactoryListener {
 
   private val settings = SexyMoveSettings.getInstance()
-
-  init {
-    EditorFactory.getInstance().addEditorFactoryListener(this, this)
-  }
 
   override fun editorCreated(event: EditorFactoryEvent) {
     val editor = event.editor
@@ -36,8 +30,5 @@ class SexyMoveManager : Disposable, EditorFactoryListener {
     }
 
     return false
-  }
-
-  override fun dispose() {
   }
 }
