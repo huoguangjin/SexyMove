e82617f6ed7809d72b5156d5d4fd7d6438695d77
huoguangjin <352886006@qq.com>
2025-03-14 08:23:16 +0800

    feat: check editor.isOneLineMode later when editor created

--- src/main/kotlin/com/github/huoguangjin/SexyMoveManager.kt
+++ src/main/kotlin/com/github/huoguangjin/SexyMoveManager.kt
@@ -3,15 +3,16 @@ package com.github.huoguangjin
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.event.EditorFactoryEvent
 import com.intellij.openapi.editor.event.EditorFactoryListener
+import com.intellij.util.application
 
 class SexyMoveManager : EditorFactoryListener {
 
   private val settings = SexyMoveSettings.getInstance()
 
-  override fun editorCreated(event: EditorFactoryEvent) {
+  override fun editorCreated(event: EditorFactoryEvent) = application.invokeLater {
     val editor = event.editor
     if (editor.isOneLineMode) {
-      return
+      return@invokeLater
     }
 
     if (shouldActivate(editor)) {
